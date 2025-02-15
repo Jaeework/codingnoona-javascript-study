552c97f374482ab9683a7c2533cfcbe6769084c5
Jaeework <jaeework@gmail.com>
2025-02-15 23:03:39 +0900

    feat: Add random number generator and link script in HTML
    1. main.js에 랜덤 번호 생성 기능 구현
    2. index.html에 main.js 연결

---
+++ number-guess-game/main.js
@@ -0,0 +1,18 @@
+// 랜덤번호 지정
+// 유저가 번호를 입력한다 그리고 go 라는 버튼을 누름
+// 만약에 유저가 랜덤번호를 맞추면, 맞췄습니다!
+// 랜덤번호가 < 유저번호 Down!!
+// 랜덤번호가 > 유저번호 Up!!
+// Reset버튼을 누르면 게임이 리셋된다.
+// 5번의 기회를 다쓰면 게임이 끝남 (더이상 추측 불가, 버튼이 disable )
+// 유저가 1~100 범위 밖에 숫자를 입력하면 알림, 기회를 깍지 않음
+// 유저가 이미 입력한 숫자를 또 입력하면 알림, 기회를 깍지 않음
+
+let randomNumber = 0;
+
+function generateRandomNumber() {
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log('정답', randomNumber);
+}
+
+generateRandomNumber();
