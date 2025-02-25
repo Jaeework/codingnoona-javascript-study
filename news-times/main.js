16e253222db2114ae6ce625ce3f49eabcc1ee90b
Jaeework <jaeework@gmail.com>
2025-02-25 19:19:43 +0900

    feat: Render NewsAPI data in HTML
    - main.js에서 NewsAPI를 통해 받아온 데이터를 HTML에 동적으로 표시하는 기능 구현
    - 각 뉴스 기사가 화면에 렌더링되도록 코드 추가

--- news-times/main.js
+++ news-times/main.js
@@ -1,4 +1,4 @@
-let news = [];
+let newsList = [];
 const getLatestNews = async () => {
     const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`);
 
@@ -6,10 +6,33 @@ const getLatestNews = async () => {
 
     const response = await fetch(url);
     const data = await response.json();
-    news = data.articles;
+    newsList = data.articles;
 
-    console.log("ddd", news);
-}
+    console.log("ddd", newsList);
+
+    render();
+};
+
+const render = () => {
+    const newsHTML = newsList.map((news) => {
+                return  `<div class="row news">
+                            <div class="col col-lg-4">
+                                <img class="news-img-size" src="${news.urlToImage}">
+                            </div>
+                            <div class="col col-lg-8">
+                                <h2>${news.title}</h2>
+                                <p>
+                                    ${news.description}
+                                </p>
+                                <div>
+                                    ${news.source.name} * ${news.publishedAt}
+                                </div>
+                            </div>
+                        </div>`
+    }).join("");
+
+    document.getElementById("news-board").innerHTML = newsHTML;
+};
 
 getLatestNews();
 
